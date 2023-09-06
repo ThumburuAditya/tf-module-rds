c37b02ec2dbb099e33fa8427e21b505220289f72
ThumburuAditya <chandramouliadityat@gmail.com>
2023-09-06 18:18:41 +0530

    Adding the code

--- vars.tf
+++ vars.tf
@@ -9,7 +9,8 @@ variable "allow_db_cidr" {}
 variable "engine_version" {}
 variable "kms_arn" {}
 variable "port_no" {
-  default = "3306"
+  default = 3306
 }
 variable "instance_count" {}
 variable "instance_class" {}
+
